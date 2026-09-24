d234d8b89f9810d7a1a1cc7c189ae2ce0e0ec270
ZawHein337 <www.zawhein337@gmail.com>
2026-09-24 17:29:07 +0630

    Update app_constants.dart

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -3,7 +3,7 @@ import 'package:stackfood_multivendor_restaurant/util/images.dart';
 
 class AppConstants {
   static const String appName = 'MyanFood Partner';
-  static const double appVersion = 9.3; ///Flutter SDK: 3.47.2
+  static const double appVersion = 2.1; ///Flutter SDK: 3.47.2
 
   static const String baseUrl = 'https://www.myanfoodmm.com';
   static const String configUri = '/api/v1/config';
